93ae64a221c98dc3d9cae529c3fd1ed5bff3e472
TalhaYasir210 <itsepic-1@gmail.com>
2026-06-15 11:32:23 +0500

    #2 feat: implement first publisher node (close #2)

---
+++ build/go_to_pose_pkg/CMakeFiles/first_publisher.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/first_publisher.dir/src/first_publisher.cpp.o"
+  "CMakeFiles/first_publisher.dir/src/first_publisher.cpp.o.d"
+  "first_publisher"
+  "first_publisher.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/first_publisher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
